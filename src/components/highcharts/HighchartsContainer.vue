378fcb25f79942a463fcc55f9642ecab41c47bdc
wenka <wkwenka@gmail.com>
2018-06-13 11:03:59 +0800

    添加 highcharts

---
+++ src/components/highcharts/HighchartsContainer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="marginAuto"></div>
+</template>
+
+<script>
+  import Highcharts from 'highcharts/highstock';
+  import HighchartsMore from 'highcharts/highcharts-more';
+  import HighchartsDrilldown from 'highcharts/modules/drilldown';
+  import Highcharts3D from 'highcharts/highcharts-3d';
+
+  HighchartsMore(Highcharts)
+  HighchartsDrilldown(Highcharts);
+  Highcharts3D(Highcharts);
+
+  export default {
+    props: ['options'],
+    name: "HighchartsContainer",
+    data() {
+      return {
+        chart: null
+      }
+    },
+    mounted() {
+      this.initChart();
+    },
+    methods: {
+      initChart() {
+        this.options.credits = {
+          enabled: false
+        }
+        this.chart = new Highcharts.Chart(this.$el, this.options);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
